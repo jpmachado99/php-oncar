e726d8bd5bb1db13239a79b9229a0161af7e2f8b
JOAO PEDRO MACHADO <joao.machado@embras.net>
2020-10-21 21:59:02 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+error_reporting(E_ALL ^ E_NOTICE);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Teste PHP - OnCar</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Menu</a>
+			</div>
+
+			<div>
+				<ul class="nav navbar-nav">
+					<li><a href="produto-formulario.php">Adicionar Produtos</a></li>
+					<li><a href="produto-lista.php">Lista de Produtos</a></li>
+					<li><a href="contato.php">Contato</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		
+		<div class="principal">
+
+        </div>
+    </div>
